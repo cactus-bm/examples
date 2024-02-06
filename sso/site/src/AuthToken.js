7ed4dba55c34a53ab0ec3c9bcd0be57c3267074c
James Robinson <github@epii.co.uk>
2024-02-06 16:56:16 -0400

    Improve auth token.

--- sso/site/src/AuthToken.js
+++ sso/site/src/AuthToken.js
@@ -1,56 +1,59 @@
-import jwt from 'jsonwebtoken';
-
 class AuthToken {
-    static setToken(token) {
-        console.log(token)
-        localStorage.setItem("token.access_token", token.access_token);
-        localStorage.setItem("token.expires_at", (token.expires_in - 30) * 1000 + new Date().getTime());
-        localStorage.setItem("token.id_token", token.id_token);
-        localStorage.setItem("token.refresh_token", token.refresh_token);
-        localStorage.setItem("token.token_type", token.token_type);
-    }
+  static setToken(token) {
+    localStorage.setItem("token.access_token", token.access_token);
+    localStorage.setItem(
+      "token.expires_at",
+      (token.expires_in - 30) * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("token.id_token", token.id_token);
+    localStorage.setItem("token.refresh_token", token.refresh_token);
+    localStorage.setItem("token.token_type", token.token_type);
+  }
 
-    static hasToken() {
-        return localStorage.getItem("token.access_token") != null;
-    }
+  static hasToken() {
+    return !!localStorage.getItem("token.access_token");
+  }
 
-    static getToken() {
-        if (!AuthToken.hasToken()) {
-            return null;
-        }
-        return {
-            access_token: localStorage.getItem("token.access_token"),
-            expires_at: new Date(parseInt(localStorage.getItem("token.expires_at"))),
-            id_token: localStorage.getItem("token.id_token"),
-            refresh_token: localStorage.getItem("token.refresh_token"),
-            token_type: localStorage.getItem("token.token_type")
-        };
-    }
+  static tokenExpired() {
+    return (
+      !!localStorage.getItem("token.expires_at") &&
+      (new Date(parseInt(localStorage.getItem("token.expires_at"))) > new Date())
+    );
 
-    static getClaims() {
-        const token = this.getToken();
-        if (token) {
-            var parts = token.id_token.split('.');
-            if (parts.length !== 3) {
-                throw new Error('JWT must have 3 parts');
-            }
+  }
 
-            // Decode the Header and Payload
-            const header = JSON.parse(atob(parts[0].replace(/_/g, '/').replace(/-/g, '+')));
-            const payload = JSON.parse(atob(parts[1].replace(/_/g, '/').replace(/-/g, '+')));
+  static getAccessToken() {
+    localStorage.getItem("token.access_token");
+  }
 
-            return { header, payload };
-        }
+  static getClaims() {
+    const id_token = localStorage.getItem("token.id_token");
+    if (id_token) {
+      var parts = id_token.split(".");
+      if (parts.length !== 3) {
         return null;
-    }
+      }
 
-    static clearToken() {
-        localStorage.setItem("token.access_token", null);
-        localStorage.setItem("token.expires_at", null);
-        localStorage.setItem("token.id_token", null);
-        localStorage.setItem("token.refresh_token", null);
-        localStorage.setItem("token.token_type", null);
+      const payload = JSON.parse(
+        atob(parts[1].replace(/_/g, "/").replace(/-/g, "+"))
+      );
+
+      return payload;
     }
+    return null;
+  }
+
+  static getRefreshToken() {
+    return localStorage.getItem("token.refresh_token");
+  }
+
+  static clearToken() {
+    localStorage.removeItem("token.access_token");
+    localStorage.removeItem("token.expires_at");
+    localStorage.removeItem("token.id_token");
+    localStorage.removeItem("token.refresh_token");
+    localStorage.removeItem("token.token_type");
+  }
 }
 
 export default AuthToken;

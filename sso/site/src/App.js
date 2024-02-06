32f9d469eadc064e19d44af8d0e196e92935b405
James Robinson <github@epii.co.uk>
2024-02-05 20:04:41 -0400

    If not logged in go to hosted page, else load the token from the code.

--- sso/site/src/App.js
+++ sso/site/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useFetchAuthTokenFromCodeQuery } from "services/authService";
+import * as config from "config";
 
-function App() {
+const redirectToLogin = () => {
+  window.location.href =
+    `https://${config.getOAuthDomain()}/oauth2/authorize?` +
+    new URLSearchParams({
+      scope: "email openid",
+      response_type: "code",
+      client_id: config.getClientId(),
+      redirect_uri: config.getLocalUrl(),
+    });
+};
+
+const App = () => {
+  const code = new URLSearchParams(window.location.search).get("code");
+  const {
+    data: token,
+    isLoading,
+    isError,
+  } = useFetchAuthTokenFromCodeQuery({ code }, { skip: !code });
+
+  if (!code && !token) {
+    redirectToLogin();
+  }
+
+  if (isError) {
+    return <div>Something went wrong</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log("token", token);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You are logged in.
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;

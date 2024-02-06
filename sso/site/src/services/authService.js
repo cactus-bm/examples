c45d4fee75db8ed6b6f8be0ddced1ea02cba511b
James Robinson <github@epii.co.uk>
2024-02-05 20:03:10 -0400

    Make a basic service to query cognito.

--- sso/site/src/services/authService.js
+++ sso/site/src/services/authService.js
@@ -5,33 +5,26 @@ import * as config from "config";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://${config.getOAuthDomain}/`,
+    baseUrl: `https://${config.getOAuthDomain()}/`,
     prepareHeaders: (headers) => {
       headers.set("content-type", "application/x-www-form-urlencoded");
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    fetchAuthTokenFromCode: builder.mutation({
-      query: ({ code }) => {
-        const formData = new URLSearchParams();
-        Object.entries({
+    fetchAuthTokenFromCode: builder.query({
+      query: ({ code }) => ({
+        url: "oauth2/token",
+        method: "POST",
+        body: new URLSearchParams({
           grant_type: "authorization_code",
           client_id: config.getClientId(),
           code,
           redirect_uri: config.getLocalUrl(),
-        }).forEach(([key, value]) => {
-          formData.append(key, value);
-        });
-
-        return {
-          url: "oauth2/token",           
-          method: "POST",
-          body: formData,
-        };
-      },
+        }),
+      }),
     }),
   }),
 });
 
-export const { useFetchAuthTokenFromCodeMutation } = authApi;
+export const { useFetchAuthTokenFromCodeQuery } = authApi;

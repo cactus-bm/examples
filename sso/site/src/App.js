1e6e7e41718113b898f0cad60ecf90adeb537133
James Robinson <github@epii.co.uk>
2024-02-06 11:31:44 -0400

    Move token to localStorage.

--- sso/site/src/App.js
+++ sso/site/src/App.js
@@ -4,22 +4,25 @@ import { useFetchAuthTokenFromCodeQuery } from "services/authService";
 import * as config from "config";
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
-
 const loginHref = () => {
-  return `https://${config.getOAuthDomain()}/oauth2/authorize?` +
+  return (
+    `https://${config.getOAuthDomain()}/oauth2/authorize?` +
     new URLSearchParams({
       scope: "email openid",
       response_type: "code",
       client_id: config.getClientId(),
       redirect_uri: config.getCallbackUrl(),
-    });
+    })
+  );
 };
 
 class AuthToken {
-  static token = null;
-
   static setToken(token) {
-    this.token = token;
+    localStorage.setItem("authToken", token);
+  }
+
+  static getToken() {
+    return localStorage.getItem("authToken");
   }
 }
 
@@ -33,7 +36,7 @@ const Auth = () => {
   } = useFetchAuthTokenFromCodeQuery({ code }, { skip: !code });
   if (token) {
     AuthToken.setToken(token);
-    return <Navigate to={"/"} />
+    return <Navigate to={"/"} />;
   }
   if (isLoading) {
     return <div>Loading...</div>;
@@ -41,39 +44,44 @@ const Auth = () => {
   if (isError) {
     return <div>Error</div>;
   }
-  console.log(queryParams)
-  return <div>
-    {queryParams}
-  </div>;
-}
+  console.log(queryParams);
+  return <div>{queryParams}</div>;
+};
 
-const WhirlyGig = () => (<div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      You are logged in.
-    </a>
-  </header>
-</div>)
+const WhirlyGig = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        You are logged in.
+      </a>
+    </header>
+  </div>
+);
+
+const Redirect = ({ to }) => {
+  window.location.replace(to);
+  return <></>;
+};
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"auth/*"} element={<Auth />} />
-        {AuthToken.token ? (
+        <Route path={"auth"} element={<Auth />} />
+        {AuthToken.getToken() ? (
           <Route path={"*"} element={<WhirlyGig />} />
         ) : (
-          <Route path={"*"} element={<Navigate  />} />
+          <Route path={"*"} element={<Redirect to={loginHref()} />} />
         )}
       </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;

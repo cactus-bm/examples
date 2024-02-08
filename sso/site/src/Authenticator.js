739164b663ea511c2fe0e4f079e9b3e097f9bbb2
James Robinson <github@epii.co.uk>
2024-02-08 18:11:31 -0400

    Create a simple authenticator with basic sign in and sign out functionality.

--- sso/site/src/Authenticator.js
+++ sso/site/src/Authenticator.js
@@ -1,74 +1,85 @@
-
-import { useFetchAuthTokenFromCodeQuery } from "services/authService";
-import * as config from "config";
-import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Loading from "components/Loading";
 import Error from "components/Error";
-import AuthToken from "AuthToken";
+import { Auth } from "aws-amplify";
+import { useState } from "react";
 
-const loginHref = () => {
-  return (
-    `https://${config.getOAuthDomain()}/oauth2/authorize?` +
-    new URLSearchParams({
-      scope: "email openid",
-      response_type: "code",
-      client_id: config.getClientId(),
-      redirect_uri: config.getCallbackUrl(),
-    })
-  );
-};
+const MAX_RETRIES = 3;
 
-const Auth = () => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const code = queryParams.get("code");
-  const {
-    data: token,
-    isLoading,
-    isError,
-  } = useFetchAuthTokenFromCodeQuery({ code }, { skip: !code });
-  if (queryParams.has("error")) {
-    return <Error
-      title={"Unable to get login code: " + queryParams.get("error")}
-      description={queryParams.get("error_description")}
-    />;
-  }
-  if (isLoading) {
-    return <Loading />;
-  }
-  if (isError) {
-    return <Error
-      title={"Unable to load token from code"}
-      description={JSON.stringify(token, null, 2)}
-    />;
-  }
-  if (token) {
-    AuthToken.setToken(token);
-    return <Navigate to={"/"} />;
-  }
-};
-
-const Redirect = ({ to }) => {
-  window.location.replace(to);
-  return <></>;
+const SignIn = () => {
+  Auth.federatedSignIn();
+  return <Loading />;
 };
 
 const SignOut = () => {
-  AuthToken.clearToken();
-  return <Navigate to={"/"} />;
+  const [loading, setLoading] = useState(true);
+  Auth.signOut().then(() => {
+    setLoading(false);
+  });
+  if (loading) {
+    return <Loading />;
+  }
+  return <>{"You have been signed out."}</>;
+};
+
+const Validator = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [count, setCount] = useState(0);
+
+  Auth.currentSession().then((session) => {
+    console.log("currentSession", session);
+  });
+  Auth.currentAuthenticatedUser().then((user) => {
+    console.log("currentAuthenticatedUser", user);
+  });
+  Auth.currentUserPoolUser().then((userPoolUser) => {
+    console.log("currentUserPoolUser", userPoolUser);
+  });
+
+  if (!loading && !user && !error) {
+    setLoading(true);
+    Auth.currentAuthenticatedUser().then((currentAuthenticatedUser) => {
+      if (currentAuthenticatedUser == null) {
+        if (count < MAX_RETRIES) {
+          setCount(count + 1);
+          Auth.federatedSignIn();
+        } else {
+          setLoading(false);
+          setError(`Failed to log you in after ${MAX_RETRIES} attempts.`);
+        }
+      } else {
+        setLoading(false);
+        setUser(currentAuthenticatedUser);
+      }
+    });
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error title={"Unable to log you in."} description={error} />;
+  }
+  if (user) {
+    return <>{children}</>;
+  }
+  return (
+    <Error
+      title={"You are not authenticated."}
+      description={"Some how you are not authenticated. We do not know why."}
+    />
+  );
 };
 
 const Authenticator = ({ children }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"auth"} element={<Auth />} />
-        <Route path={"login"} element={<Redirect to={loginHref()} />} />
-        <Route path={"logoff"} element={<SignOut />} />
-        {AuthToken.getToken() ? (
-          <Route path={"*"} element={<>{children}</>} />
-        ) : (
-          <Route path={"*"} element={<Redirect to={loginHref()} />} />
-        )}
+        <Route path={"signin"} element={<SignIn />} />
+        <Route path={"signout"} element={<SignOut />} />
+        <Route path={"*"} element={<Validator children={children} />} />
       </Routes>
     </BrowserRouter>
   );

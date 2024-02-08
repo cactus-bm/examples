4049ada221a365dd58d7c5c0bf16776dbc1c604d
James Robinson <github@epii.co.uk>
2024-02-08 19:44:18 -0400

    Remove /auth from the callback.

--- sso/cognito/setup-userpool.sh
+++ sso/cognito/setup-userpool.sh
@@ -275,7 +275,7 @@ echo
 CALLBACK_URL=${INPUT:-$DEFAULT_CALLBACK}
 if [[ "$CALLBACK_URL" =~ ^[0-9]+$ ]]; then
   CHOSEN_PORT=$CALLBACK_URL
-  CALLBACK_URL=https://localhost:$CALLBACK_URL/auth
+  CALLBACK_URL=https://localhost:$CALLBACK_URL/
 fi
 
 echo "Using $CALLBACK_URL as the callback url."

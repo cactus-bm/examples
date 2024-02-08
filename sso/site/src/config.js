05aa24403751355f6bf769aae7b3fbf6449a7095
James Robinson <github@epii.co.uk>
2024-02-08 18:09:52 -0400

    Update the config and index to load in the config required to let AWS Amplify library handle Auth.

--- sso/site/src/config.js
+++ sso/site/src/config.js
@@ -1,16 +1,20 @@
-const OAUTH_DOMAIN = process.env.REACT_APP_OAUTH_DOMAIN;
 const USERPOOL_ID = process.env.REACT_APP_USERPOOL_ID;
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const REGION = process.env.REACT_APP_REGION;
-const CALLBACK_URL = process.env.REACT_APP_CALLBACK_URL;
-
-export const getOAuthDomain = () => OAUTH_DOMAIN;
-
-export const getUserPoolId = () => USERPOOL_ID;
-
-export const getClientId = () => CLIENT_ID;
+const OAUTH_DOMAIN = process.env.REACT_APP_OAUTH_DOMAIN;
+const OAUTH_CALLBACK_URL = process.env.REACT_APP_OAUTH_CALLBACK_URL;
+const OAUTH_SIGNOUT_URL = process.env.REACT_APP_OAUTH_SIGNOUT_URL;
+const OAUTH_SCOPE = process.env.REACT_APP_OAUTH_SCOPE;
+const OAUTH_RESPONSE_TYPE = process.env.REACT_APP_OAUTH_RESPONSE_TYPE;
 
 export const getRegion = () => REGION;
+export const getUserPoolId = () => USERPOOL_ID;
+export const getClientId = () => CLIENT_ID;
 
-export const getCallbackUrl = () => CALLBACK_URL;
-
+export const getOAuth= () => ({
+    domain: OAUTH_DOMAIN,
+    scope: OAUTH_SCOPE,
+    redirectSignIn: OAUTH_CALLBACK_URL,
+    redirectSignOut: OAUTH_SIGNOUT_URL,
+    responseType: OAUTH_RESPONSE_TYPE,
+});

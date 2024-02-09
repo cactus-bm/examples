4bdbef534c2dd1e41250af3a0d31975c34b61b02
James Robinson <github@epii.co.uk>
2024-02-08 22:59:51 -0400

    Add Buttons to the Error Dialog.

--- sso/site/src/components/Error.js
+++ sso/site/src/components/Error.js
@@ -1,31 +1,64 @@
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
 import { Alert, Grid } from "@mui/material";
 
-const Error = ({ title, description }) => {
-    return <Dialog
-        open={true}
-        aria-labelledby={"error-dialog-title"}
-        maxWidth={"lg"}
-        fullWidth
-        scroll={"paper"}
-        className={"error-dialog"}
+const Error = ({ title, description, buttons }) => {
+  return (
+    <Dialog
+      open={true}
+      aria-labelledby={"error-dialog-title"}
+      maxWidth={"lg"}
+      fullWidth
+      scroll={"paper"}
+      className={"error-dialog"}
     >
-        <DialogTitle id={"error-dialog-title"}>
-            {title || "Error"}
-        </DialogTitle>
-        <DialogContent dividers={true}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} >
-                    <Alert severity="error">
-                        {description || "An error has occured"}
-                    </Alert>
+      <DialogTitle id={"error-dialog-title"}>{title || "Error"}</DialogTitle>
+      <DialogContent>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Alert severity="error">
+              {description || "An error has occured"}
+            </Alert>
+          </Grid>
+        </Grid>
+      </DialogContent>
+      {buttons && (
+        <DialogActions
+          sx={{
+            justifyContent: "space-between",
+            padding: "0 24px 18px 24px",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid
+              item
+              container
+              spacing={2}
+              direction={"row"}
+              xs={12}
+              justifyContent="flex-end"
+            >
+              {buttons.map((button, index) => (
+                <Grid item xs={"auto"} key={index}>
+                  <Button
+                    disableElevation
+                    variant={button.variant}
+                    color={button.color}
+                    onClick={button.onClick}
+                  >
+                    {button.label}
+                  </Button>
                 </Grid>
+              ))}
             </Grid>
-
-        </DialogContent>
+          </Grid>
+        </DialogActions>
+      )}
     </Dialog>
+  );
 };
 
 export default Error;
